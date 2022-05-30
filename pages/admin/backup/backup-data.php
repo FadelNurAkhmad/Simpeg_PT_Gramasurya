88b5299587c7010b9dcda14f7ba4b2f35d718f26
burhanusr <bsultanramadan@gmail.com>
2022-05-31 00:08:29 +0700

    perubahan mysqli backup-data bagian admin

--- pages/admin/backup/backup-data.php
+++ pages/admin/backup/backup-data.php
@@ -31,12 +31,12 @@
 	*/
 	function backup_tables($host,$user,$pass,$name,$nama_file,$tables ='*')	{
 		$link = mysqli_connect($host,$user,$pass);
-		mysqli_select_db($name,$link);
+		mysqli_select_db($link, $name);
 			
 		if($tables == '*'){
 			$tables = array();
-			$result = mysqli_query('SHOW TABLES');
-			while($row = mysqli_fetch_row($result, MYSQLI_ASSOC)){
+			$result = mysqli_query($link, 'SHOW TABLES');
+			while($row = mysqli_fetch_row($result)){
 				$tables[] = $row[0];
 			}
 		}
@@ -45,15 +45,15 @@
 		}
 				
 		foreach($tables as $table){
-			$result = mysqli_query('SELECT * FROM '.$table);
+			$result = mysqli_query($link, 'SELECT * FROM '.$table);
 			$num_fields = mysqli_num_fields($result);
 																
 			$return.= 'DROP TABLE '.$table.';'; //menyisipkan query drop table untuk nanti hapus table yang lama
-			$row2 = mysqli_fetch_row(mysqli_query('SHOW CREATE TABLE '.$table), MYSQLI_ASSOC);
+			$row2 = mysqli_fetch_row(mysqli_query($link, 'SHOW CREATE TABLE '.$table));
 			$return.= "\n\n".$row2[1].";\n\n";
 					
 			for ($i = 0; $i < $num_fields; $i++) {
-				while($row = mysql_fetch_row($result)){	//menyisipkan query Insert. untuk nanti memasukan data yang lama ketable yang baru dibuat
+				while($row = mysqli_fetch_row($result)){	//menyisipkan query Insert. untuk nanti memasukan data yang lama ketable yang baru dibuat
 				$return.= 'INSERT INTO '.$table.' VALUES(';
 					for($j=0; $j<$num_fields; $j++) { //akan menelusuri setiap baris query didalam
 						$row[$j] = addslashes($row[$j]);
